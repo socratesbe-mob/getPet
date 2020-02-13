5a3d4d1e690a2127d1556fbb78807645b7a0b439
Davy Kenis <davy.kenis@nike.com>
2020-02-13 13:09:35 +0100

    Adding getPet API nodejs code

---
+++ app.js
@@ -0,0 +1,17 @@
+exports.handler = async (event) => {
+    // TODO implement
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify(
+        {
+        message: 'getPet API',
+        names: ['Tom','Jerry','Spike'],
+        date: new Date()
+        }
+            
+            
+            ),
+    };
+    return response;
+};
+
